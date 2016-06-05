1adeeb7f3611f0391595fe323ac3f4419d7180d8
Richard Kraaijenhagen <stdin+git@riichard.com>
2016-06-05 14:59:20 +0200

    Backu!

--- app.js
+++ app.js
@@ -11,13 +11,17 @@ app.get('/', function(req, res) {
     res.send('Hello ' + req.param('name', 'world'));
 });
 
+// Store ALL the todos
 app.post('/todos.json', function(req, res) {
     console.log('writing todos.json');
+    console.log('request body', req.body);
 
+    // Turn req.body into a string
     var jsonBody = JSON.stringify(req.body, null, '    ');
 
     console.log('HELLO A');
 
+    // write that string to todos.json
     fs.writeFile('todos.json', jsonBody, function(err) {
         console.log('HELLO B');
         if (err) {
@@ -33,11 +37,39 @@ app.post('/todos.json', function(req, res) {
     console.log('HELLO C');
 });
 
+
+// Getting ALL todos
 app.get('/todos.json', function(req, res) {
     console.log('getting todos.json');
 
     fs.readFile('todos.json', 'utf8', function(err, todos) {
         res.send(todos);
+    });
+});
+
+
+// Saving a SINGLE todo
+app.put('/todos.json', function(req, res) {
+    var todo = req.body;
+    console.log(req.body);
+
+    // read todos
+    fs.readFile('todos.json', 'utf8', function(err, todosJson) {
+        // Convert todos.json to an object
+        var todos = JSON.parse(todosJson);
+
+        // push todo to todos array
+        todos.push(todo);
+
+        // Convert todos back to JSON string
+        var todosJson = JSON.stringify(todos);
+
+        // write todos to todos.json
+        fs.writeFile('todos.json', todosJson, function() {
+
+            // Send back the changed todos to the user
+            res.send(todos);
+        });
     });
 });
 
--- chef.js
+++ chef.js
@@ -1,10 +1,11 @@
-function helpCustomer(name) {
+function helpCustomer(name, foodDone) {
     console.log('Going to help ' + name);
 
     // This function will be called ofter 5 seconds (5000 miliseconds)
     setTimeout(function(){
 
         console.log('food done for ' + name);
+        foodDone(name); // Call the callback function
 
     }, 5000);
 }
@@ -13,9 +14,7 @@ function startEating(name) {
     console.log(name + ' will now start eating');
 }
 
-helpCustomer('Samir');
-startEating('Samir');
-helpCustomer('Marwa');
-startEating('Marwa');
-helpCustomer('Hadi');
-startEating('Hadi');
+helpCustomer('Samir', startEating);
+helpCustomer('Marwa', startEating);
+helpCustomer('Hadi', startEating);
+
